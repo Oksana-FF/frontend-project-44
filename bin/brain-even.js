e47d59b1c6ea6c8e906a882b0483ad501f14020e
Oksana Fatkhulova <fato1284@gmail.com>
2023-05-16 21:41:33 +0300

    update Makefile and package.json

---
+++ bin/brain-even.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+
+import greeting from '../src/cli.js'; 
+
+console.log('Welcome to the Brain Games!');
+greeting();
+console.log('Answer "yes" if the number is even, otherwise answer "no".');
