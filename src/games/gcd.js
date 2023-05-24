778bae2d99a29578060b687876b7db94fcd4dfd3
Oksana Fatkhulova <fato1284@gmail.com>
2023-05-24 15:29:06 +0300

    update bin/brain-gcd.js and src/games/gcd.js

---
+++ src/games/gcd.js
@@ -0,0 +1,33 @@
+import getWelcome from '../index.js';
+import readlineSync from 'readline-sync';
+
+function NOD(x, y) {
+    if (y > x) return NOD(y, x);
+    if (!y) return x;
+    return NOD(y, x % y);
+}
+
+export default function gcdGame() {
+ const userName = getWelcome();
+ console.log('Find the greatest common divisor of given numbers');
+
+ for (let i = 0; i < 3; i += 1) {
+
+  let runNumber1 = Math.floor(Math.random(1, 10) * (10 - 1 + 1)) + 1;
+  let runNumber2 = Math.floor(Math.random(1, 10) * (10 - 1 + 1)) + 1;
+  
+  console.log(`Question: ${runNumber1} ${runNumber2}`); 
+        
+  const userAnswer = readlineSync.question('Your answer: ');
+     
+  const correctAnswer = NOD(runNumber1, runNumber2);
+
+ if (Number(userAnswer) === correctAnswer) {
+    console.log('Correct!');
+    } else { 
+    console.log(`${userAnswer} is wrong answer ;( Correct answer was '${correctAnswer}'. \nLet's try again, ${userName}!`);
+    return ;
+    }
+ }
+ console.log(`Congratulation, ${userName}!`);
+}
