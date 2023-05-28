9730b6bc2bfef4652bf907b19cd426992fa4642a
Oksana Fatkhulova <fato1284@gmail.com>
2023-05-28 19:31:52 +0300

    update src/games/even.js

--- src/games/even.js
+++ src/games/even.js
@@ -6,21 +6,31 @@ export default function evenGame() {
  const userName = getWelcome();
  console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
+ function evenNumber(number) {
+  let result = '';
+  if (number % 2 === 0) {
+    result = true;
+  }
+  return result;
+ }
+
  for (let i = 0; i < 3; i += 1) {
 
   const runNumber = Math.floor(Math.random(1, 100) * (100 - 1 + 1)) + 1;
   console.log(`Question: ${runNumber}`); 
         
   const userAnswer = readlineSync.question('Your answer: ');
+
+  const correctAnswer = evenNumber(runNumber) ? 'yes' : 'no';
         
-  if ((runNumber % 2 === 0 && userAnswer === 'yes') || (runNumber % 2 !== 0 && userAnswer === 'no')) {
+  if (userAnswer === correctAnswer) {
     console.log('Correct!');
-  } else {
-    console.log(`${userAnswer} is wrong answer ;( Correct answer was 'yes'. \nLet's try again, ${userName}!`);
+    } else { 
+    console.log(`${userAnswer} is wrong answer ;( Correct answer was '${correctAnswer}'. \nLet's try again, ${userName}!`);
     return ;
     }
-    
  }
+ 
  console.log(`Congratulation, ${userName}!`);
 }
   
